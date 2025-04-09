6fb67e7486da803473fdf9acf5380d4de430a583
Pedro Duarte <pduartesilva2005@gmail.com>
2025-04-09 20:32:38 -0300

    Format the currency

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
 // Seleciona os elementos do formulário
 const amount = document.getElementById("amount");
 
+// Captura o evento de input para formatar o valor.
 amount.oninput = () => {
+  // Obtém o valor atual do input e remove os caracteres não númericos.
   let value = amount.value.replace(/\D/g, "");
 
-  amount.value = value;
+  // Transforma o valor em centavos.
+  value = Number(value) / 100;
+
+  // Atualiza o valor do input
+  amount.value = formatCurrencyBRL(value);
 };
+
+function formatCurrencyBRL(value) {
+  // Formata o valor no padrão BRL (Real Brasileiro)
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  return value;
+}
