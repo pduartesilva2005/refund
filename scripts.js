796625b65a481eb3e29207897502f6271b4a10ad
Pedro Duarte <pduartesilva2005@gmail.com>
2025-04-15 20:10:05 -0300

    Create the remove icon

--- scripts.js
+++ scripts.js
@@ -81,8 +81,14 @@ function expenseAdd(newExpense) {
       .toUpperCase()
       .replace("R$", "")}`;
 
+    // Cria o ícone de remover
+    const removeIcon = document.createElement("img");
+    removeIcon.classList.add("remove-icon");
+    removeIcon.setAttribute("src", "img/remove.svg");
+    removeIcon.setAttribute("alt", "remover");
+
     // Adiciona as informações no item
-    expenseItem.append(expenseIcon, expenseInfo, expenseAmount);
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
 
     // Adiciona o item na lista.
     expenseList.append(expenseItem);
