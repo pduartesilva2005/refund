9d09baca531af6f32e201ae35a17d878bf20c5a2
Pedro Duarte <pduartesilva2005@gmail.com>
2025-04-17 20:09:57 -0300

    Taking the usability

--- scripts.js
+++ scripts.js
@@ -96,6 +96,9 @@ function expenseAdd(newExpense) {
     // Adiciona o item na lista.
     expenseList.append(expenseItem);
 
+    // Limpa o formulário para adicionar um novo item.
+    formClear();
+
     // Atualiza os totais.
     updateTotals();
   } catch (error) {
@@ -173,3 +176,12 @@ expenseList.addEventListener("click", (event) => {
   // Atualiza os totais.
   updateTotals();
 });
+
+function formClear() {
+  // Limpa os inputs.
+  expense.value = "";
+  category.value = "";
+  amount.value = "";
+
+  expense.focus();
+}
